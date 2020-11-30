29415f7fc9581f2b1f0673ecb9b2030797ef1ea3
Awtanabe <panser098@gmail.com>
2020-11-30 22:25:47 +0900

    非同期でメッセージを送れる様に

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,13 @@ class MessagesController < ApplicationController
 
   def create
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(:user_id => current_user.id))
+      @message = Message.create(message_params)
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :message, :room_id).merge(:user_id => current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   post 'like/:id' => 'likes#create', as: 'create_like'
